a28ff31979f64230751d6fc2aa3e4020cc97b001
magantus@googlemail.com <Extrem93!>
2022-03-30 01:22:07 +0200

    update

---
+++ backend/src/main/java/fh/dortmund/backend/controller/TestController.java
@@ -0,0 +1,12 @@
+package fh.dortmund.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/")
+    public String getTest(){
+        return "Test";
+    }
+}
